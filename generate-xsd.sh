45c9ab56bc96b7e2a39bad7c42c501f0d99a39ff
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2021-04-16 14:51:28 +0200

    Generate ExternalFileReference

--- generate-xsd.sh
+++ generate-xsd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 xsltproc src/main/xslt/addAnnotations.xsl src/main/resources/xsd/ISO11783_Common_V4-3.xsd > src/main/xsd/ISO11783_Common_V4-3.xsd
 xsltproc src/main/xslt/addAnnotations.xsl src/main/resources/xsd/ISO11783_TaskFile_V4-3.xsd > src/main/xsd/ISO11783_TaskFile_V4-3.xsd
+xsltproc src/main/xslt/addAnnotations.xsl src/main/resources/xsd/ISO11783_ExternalFile_V4-3.xsd > src/main/xsd/ISO11783_TaskFile_V4-3.xsd
